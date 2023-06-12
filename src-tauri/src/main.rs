74c8dd3a757017e3169dad77a22b7c3aa2672422
jokorone <jokroneberg@gmail.com>
2023-06-12 12:23:15 +0200

    set tauri allowlist, add basic menu setup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{Submenu, CustomMenuItem, Menu, MenuItem, Manager};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -9,7 +11,78 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    .invoke_handler(tauri::generate_handler![greet])
+    .menu(build_menu())
+    .setup(|app| {
+        let main_window = app.get_window("main").unwrap();
+        let window_ref = main_window.clone();
+
+        // db.print_schema().expect("Failed to print schema");
+
+        main_window.on_menu_event(move |e| {
+            println!("menu event: {:?}", e.menu_item_id());
+
+            match e.menu_item_id() {
+                "open_import_dialog" => {
+                    window_ref.emit("open_import_dialog", {}).unwrap();
+                },
+                // "open_file" => {
+                //     window_ref.emit("open_file", {}).unwrap();
+                // },
+                "clear_db" => {
+
+                },
+                "reload_window" => {
+                },
+                "hide" => {
+                    window_ref.hide().unwrap();
+                },
+                "about" => {
+                    window_ref.emit("open_about_dialog", {}).unwrap();
+                },
+                "settings" => {
+                    window_ref.emit("open_settings_dialog", {}).unwrap();
+                },
+                _ => println!("handle menu event"),
+            }
+        });
+
+        Ok(())
+    })
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
+
+
+fn build_menu() -> Menu {
+    let menu = Menu::new()
+    // .add_native_item(MenuItem::Copy)
+    .add_submenu(Submenu::new(
+        "Vibster",
+        Menu::new()
+        .add_item(CustomMenuItem::new("about", "About Vibster"))
+        .add_item(
+            CustomMenuItem::new("settings", "Settings").accelerator("CommandOrControl+,"),
+        )
+        .add_native_item(MenuItem::Hide)
+        .add_native_item(MenuItem::Quit),
+    ))
+    .add_item(CustomMenuItem::new("hide", "Hide"))
+    .add_submenu(Submenu::new(
+        "Import",
+        Menu::new().add_item(
+            CustomMenuItem::new(
+                "open_import_dialog".to_string(),
+                "Import from Folder"
+            ).accelerator("CommandOrControl+o"),
+        )
+    ))
+    .add_submenu(Submenu::new(
+        "DevTools",
+        Menu::new()
+        .add_item(CustomMenuItem::new("clear_db", "Clear DB"))
+        .add_item(CustomMenuItem::new("reload_window", "Reload Window")),
+    ));
+
+    menu
 }
