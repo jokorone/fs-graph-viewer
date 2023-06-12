2f02ca51274ba707238bf4bc7f081f065597b2c8
jokorone <jokroneberg@gmail.com>
2023-06-12 12:14:03 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  theme: {
+    extend: {},
+  },
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  variants: {},
+  plugins: [],
+}
